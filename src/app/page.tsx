ec6e4de3656255800c7a78f2019ff8f9079d779a
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-11-11 12:56:55 +0530

    add padding to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default async function Home() {
   }
 
   return (
-    <>
+    <div className="flex flex-col gap-2 p-4">
       <NewTransaction />
       <Expenses />
       <Transactions />
-    </>
+    </div>
   );
 }
